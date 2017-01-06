0ef6f93693041ad14b8f13b943abea6cdb3ed9cb
DenysSidorov <000scorpions000@gmail.com>
2017-01-06 16:42:03 +0200

    add combineReducer

--- src/state.js
+++ src/state.js
@@ -2,24 +2,11 @@ import {createStore} from "redux";
 import reducers from './reducers'
 let initialState = {
     issues: [],
-    repository: {},
+    // repository: {},
     counter: 0
 };
 
 const store = createStore(reducers, {} );
 
-// function reducer(state, action) {
-//     switch (action.type) {
-//         case 'INCREMENT_COUNTER':
-//             return {...state, ...{counter: (state.counter + 1)}};
-//         case 'DECREMENT_COUNTER':
-//             return {...state, ...{counter: (state.counter - 1)}};
-//         case 'RESET_COUNTER':
-//             return {...state, ...{counter: 0}};
-//         case 'LOAD_USSUES' : return {...state, ...{issues: action.payload}}
-//         default:
-//             return state;
-//     }
-// }
 
 export default store;
